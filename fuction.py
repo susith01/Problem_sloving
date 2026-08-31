dc069ab759e042530366a1fa23b8f01f17628864
susith01 <susithravichandran@gmail.com>
2026-08-31 18:26:40 +0530

    Add a new function to count_vowels in the fuction.py file. This function will take a string as input and return the number of vowels (a, e, i, o, u) present in that string.

--- fuction.py
+++ fuction.py
@@ -19,3 +19,79 @@ def while_loop(num):
         i+=1
         
 while_loop(5) #print the numbers from 1 to 5 in function        
+
+def square(num):
+    return num*num
+result = square(5)
+print(result) #print the square of a number in function
+
+
+def check_number(num):
+    if num%2==0:
+        print(f"{num} is even")
+    else:
+        print(f"{num} is odd")
+num=int(input("Enter a number: "))
+check_number(num) #print whether the number is even or odd in function
+
+
+def count_vowels(text):
+    vowels =input("Enter the vowels: ")
+    count = 0
+    for char in text:
+        if char in vowels:
+            count += 1
+    return count
+
+text = input("Enter a string: ")
+vowel_count = count_vowels(text)
+print(f"Number of vowels in '{text}': {vowel_count}") #print the number of vowels in a string in function
+
+def count_vowels(text):
+    
+    a_count=0
+    e_count=0
+    i_count=0
+    o_count=0
+    u_count=0
+    A_count=0
+    E_count=0
+    I_count=0
+    O_count=0
+    U_count=0
+    for char in text:
+        if char == 'a':
+            a_count += 1
+        elif char == 'e':
+            e_count += 1
+        elif char == 'i':
+            i_count += 1
+        elif char == 'o':
+            o_count += 1
+        elif char == 'u':
+            u_count += 1
+        elif char == 'A':
+            A_count += 1
+        elif char == 'E':
+            E_count += 1
+        elif char == 'I':
+            I_count += 1
+        elif char == 'O':
+            O_count += 1
+        elif char == 'U':
+            U_count += 1
+    return a_count, e_count, i_count, o_count, u_count, A_count, E_count, I_count, O_count, U_count
+
+text = input("Enter a string: ")
+vowels = input("Enter the vowels: ")
+a_count, e_count, i_count, o_count, u_count, A_count, E_count, I_count, O_count, U_count = count_vowels(text)
+print("a:", a_count)
+print("e:", e_count)
+print("i:", i_count)
+print("o:", o_count)
+print("u:", u_count)
+print("A:", A_count)
+print("E:", E_count)
+print("I:", I_count)
+print("O:", O_count)
+print("U:", U_count)
